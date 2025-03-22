b574e46f8a99c17aa0f957fb87cc4bc8fa2b8e57
França <tiagofranca.rita@gmail.com>
2025-03-21 21:03:39 -0300

    [feature] - commit initial

---
+++ src/main/java/br/com/franca/rest_erudio/RestErudioApplication.java
@@ -0,0 +1,13 @@
+package br.com.franca.rest_erudio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestErudioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestErudioApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/franca/rest_erudio/RestErudioApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.franca.rest_erudio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestErudioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
